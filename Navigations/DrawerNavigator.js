096e6279c3e074f0e3483010b95629d28fec8240
pabloalber84 <pabloalber84@gmail.com>
2023-05-16 21:06:42 -0700

    Update DrawerNavigator.js

--- Navigations/DrawerNavigator.js
+++ Navigations/DrawerNavigator.js
@@ -6,9 +6,6 @@ import {
 	DrawerItemList,
 } from "@react-navigation/drawer";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import Productos from "../Screens/Productos";
-import Quejas from "../Screens/Quejas";
-import Inicio from "../Screens/Inicio";
 import BottomTabNavigator from "./BottomTabNavigator";
 import {
 	ProductosStackNavigator,
@@ -17,82 +14,6 @@ import {
 
 const Drawer = createDrawerNavigator();
 
-/* export default function DrawerNavigator() {
-	const DrawerNavigatorContent = (props) => {
-		<DrawerContentScrollView {...props}>
-			<DrawerItemList {...props} />
-			<View
-				style={{ flexDirection: "row", marginLeft: 20, marginTop: 15 }}
-			>
-				<Ionicons name="md-exit" size={25} color={"#060606"} />
-				<Text
-					style={{ marginLeft: 30 }}
-					onPress={() => alert("Cerrando sesión")}
-				>
-					Cerrar sesión
-				</Text>
-			</View>
-		</DrawerContentScrollView>;
-	};
-	return (
-		<Drawer.Navigator
-			useLegacyImplementation
-			screenOptions={{
-				//drawerContentOptions
-				drawerStyle: {
-					backgroundColor: "#ffff",
-					width: "80%",
-				},
-				drawerActiveTintColor: "#ff6600", //activeTintColor
-				drawerInactiveTintColor: "#060606", //inactiveTintColor
-			}}
-			drawerContent={DrawerNavigatorContent}
-		>
-			<Drawer.Screen
-				name="Inicio"
-				component={BottomTabNavigator}
-				options={{
-					headerShown: false,
-					drawerLabel: "Inicio",
-					drawerIcon: ({ color }) => (
-						<Ionicons name={"ios-home"} size={20} color={color} />
-					),
-				}}
-			/>
-			<Drawer.Screen
-				name="Productos"
-				component={ProductosStackNavigator}
-				options={{
-					headerShown: false,
-					drawerLabel: "Productos",
-					drawerIcon: ({ color }) => (
-						<Ionicons
-							name={"list-outline"}
-							size={20}
-							color={color}
-						/>
-					),
-				}}
-			/>
-			<Drawer.Screen
-				name="Quejas"
-				component={QuejasStackNavigator}
-				options={{
-					headerShown: false,
-					drawerLabel: "Quejas",
-					drawerIcon: ({ color }) => (
-						<Ionicons
-							name={"ios-help-circle"}
-							size={20}
-							color={color}
-						/>
-					),
-				}}
-			/>
-		</Drawer.Navigator>
-	);
-}
- */
 const DrawerNavigatorContent = (props) => (
 	<DrawerContentScrollView {...props}>
 		<DrawerItemList {...props} />
